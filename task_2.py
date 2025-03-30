723357ab5eaba48a661d9eca94441dea6b85cdf0
Maks Shkafar <m.shkafar@gmail.com>
2025-03-30 15:09:09 +0200

    create dict

--- task_2.py
+++ task_2.py
@@ -6,5 +6,10 @@ def get_cats_info(path):
         with open(path, 'r', encoding='utf-8') as file:
             for line in file:
                 line = line.strip()
+                parts = line.split(',')
+
+                if len(parts) == 3:
+                    cat_id, name, age = parts
+                    cat_info = {'id' : cat_id, 'name' : name, 'age' : age}
     except FileNotFoundError:
         print('File not found')
