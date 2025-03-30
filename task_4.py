4f71e3d72e16b0e350467e3ecf96f14e94ff12e7
Maks Shkafar <m.shkafar@gmail.com>
2025-03-30 16:45:35 +0200

    add upd contacts

--- task_4.py
+++ task_4.py
@@ -4,3 +4,27 @@ def user_input(message):
     cmd = cmd.strip().lower()
     return cmd, *args
 
+def add_contacts(args, contacts):
+    if len(args) !=2:
+        return 'Помилка вводу! Введіть, будь ласка, і\'мя та номер телефону'
+    neme, phone = args
+    contacts[name] = phone
+    return 'Додайте контакти'
+
+def change_contacts(args, contacts):
+    if len(args) !=2:
+        return 'Помилка вводу! Введіть, будь ласка, і\'мя та номер телефону'
+    if name in contacts:
+        contacts[name] = phone
+        return 'Контакт оновлено'
+    else:
+        return 'Помилка оновлення контакту'
+    
+def show_phone(args, contacts):
+    if len(args) !=2:
+        return 'Помилка вводу! Введіть, будь ласка, і\'мя'
+    
+    name = args[0]
+    
+    if name in contacts:
+        return f'Контактний номер {name} - {contacts[name]}'
