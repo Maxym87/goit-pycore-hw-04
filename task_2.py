c5d1728445bf1c03f2e54d43985e7bee79a94ea9
Maks Shkafar <m.shkafar@gmail.com>
2025-03-30 15:04:29 +0200

    processing a 'file'

---
+++ task_2.py
@@ -0,0 +1,10 @@
+
+
+def get_cats_info(path):
+    cats_list = []
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            for line in file:
+                line = line.strip()
+    except FileNotFoundError:
+        print('File not found')
